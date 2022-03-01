e678aa0635e751d1776d38b345d47b0943633b4b
babacardiallo <babacardiallo595@gmail.com>
2022-03-01 18:58:07 +0100

    merging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testtree';
+
+  data = [
+    {
+      code: 'D12',
+      endDate: '2019-08-31',
+      name: 'Discount 1',
+      offersDtos: [
+        {id: 1, libelle: 'offer 1'},
+        {id: 2, libelle: 'offer 2'},
+        {id: 3, libelle: 'offer 3'}
+      ],
+      promotionCode: 'D12',
+      startDate: '2019-08-01',
+      value: 12
+    },
+    {
+      code: 'D13',
+      endDate: '2019-09-30',
+      name: 'Discount 1',
+      offersDtos: [
+        {id: 4, libelle: 'offer 4'},
+        {id: 5, libelle: 'offer 5'},
+        {id: 6, libelle: 'offer 6'}
+      ],
+      promotionCode: 'D13',
+      startDate: '2019-09-01',
+      value: 13
+    }
+  ]
 }
